778d50e7437377e47b819dfd10e2eaf3ded129d6
swislockikacper <k.swislocki96@gmail.com>
2019-03-12 18:54:13 +0100

    added examples

---
+++ Demo.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewFeatures
+{
+    class Demo
+    {
+        private int[] elements;
+
+        public Demo()
+        {
+            elements = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        }
+
+        public IEnumerable<int> ListWithYieldReturn()
+        {
+            foreach (var element in elements)
+            {
+                if (element > 5)
+                    yield return element;
+            }
+        }
+
+        public string IsIntExpression(object o)
+        {
+            if (o is null)
+                return "Object is null";
+            if (o is int i)
+                return $"Object is int = {i}";
+
+            return "It's not int object";
+        }
+
+        public ref int Find(int number)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i] == number)
+                {
+                    Console.WriteLine($"Number in array at {i} index is {elements[i]}");
+                    return ref elements[i];
+                }
+            }
+
+            throw new IndexOutOfRangeException($"Number {number} not found");
+        }
+
+        public void PrintNumbers()
+        {
+            foreach (var element in elements)
+            {
+                Console.Write($"{element}, ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public int Fibonacci(int x)
+        {
+            if (x < 0)
+                throw new ArgumentException("Number must be greather than 0", nameof(x));
+
+            return Fib(x).previous;
+
+            (int current, int previous) Fib(int i)
+            {
+                if (i == 0)
+                    return (1, 0);
+
+                var (p, pp) = Fib(i - 1);
+
+                return (p + pp, p);
+            }
+        }
+
+        public void FuncTest()
+        {
+            var x = DoMaths(Add);
+            var y = DoMaths(Subtract);
+            var z = DoMaths((int a, int b) => { return x * y; });
+
+            Console.WriteLine("Sum = " + x);
+            Console.WriteLine("Subtract = " + y);
+            Console.WriteLine("Product = " + z);
+        }
+
+        private int DoMaths(Func<int, int, int> mathFunction)
+        {
+            var x = 10;
+            var y = 5;
+
+            return mathFunction(x, y);
+        }
+
+        private int Add(int x, int y)
+            => x + y;
+
+        private int Subtract(int x, int y)
+            => x - y;
+
+        public void PrintType(object o)
+        {
+            switch (o)
+            {
+                case int i:
+                    Console.WriteLine("It's int");
+                    break;
+                case long i:
+                    Console.WriteLine("It's long");
+                    break;
+                case short i:
+                    Console.WriteLine("It's short");
+                    break;
+                case string s:
+                    Console.WriteLine("It's string");
+                    break;
+                default:
+                    Console.WriteLine("It's another type");
+                    break;
+                case null:
+                    throw new ArgumentNullException("Object cannot be null");
+            }
+        }
+
+        private IEnumerable<T> Filter<T>(IEnumerable<T> elements, Func<T, bool> filter)
+        {
+            if (elements == null)
+                throw new ArgumentNullException(nameof(elements));
+            if (filter == null)
+                throw new ArgumentNullException(nameof(filter));
+
+            return Iterator();
+
+            IEnumerable<T> Iterator()
+            {
+                foreach (var element in elements)
+                {
+                    if (filter(element))
+                        yield return element;
+                }
+            }
+        }
+
+        private bool GetElementsBiggerThan5(int element)
+            => element > 5 ? true : false;
+
+        public IEnumerable<int> GetElementsAfterFiltering()
+            => Filter(elements, GetElementsBiggerThan5);
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace NewFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demo = new Demo();
+
+            //Yield
+            var elements = demo.ListWithYieldReturn();
+
+            Console.WriteLine("YIELD");
+            foreach (var element in elements)
+            {
+                Console.Write($"{element}, ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Checking types of objects
+            Console.WriteLine("TYPES");
+
+            Console.WriteLine(demo.IsIntExpression(null));
+            Console.WriteLine(demo.IsIntExpression(3));
+            Console.WriteLine(demo.IsIntExpression("test"));
+            Console.WriteLine();
+
+            //References
+            Console.WriteLine("REFERENCES");
+
+            demo.PrintNumbers();
+
+            ref var number = ref demo.Find(1);
+            number = 5;
+            Console.WriteLine("Numbers after change in Program.cs");
+
+            demo.PrintNumbers();
+            Console.WriteLine();
+
+            //Functions
+            Console.WriteLine("FUNCTIONS");
+
+            Console.WriteLine($"Fibnacci from 5 = {demo.Fibonacci(5)}");
+            Console.WriteLine();
+
+            //Func
+            Console.WriteLine("FUNC");
+            demo.FuncTest();
+            Console.WriteLine();
+
+            //Switch types
+            Console.WriteLine("TYPES");
+            demo.PrintType(5);
+            demo.PrintType((long)5);
+            demo.PrintType((short)5);
+            demo.PrintType("test");
+
+
+            //Iterator
+            Console.WriteLine("ITERATOR");
+            foreach (var element in demo.GetElementsAfterFiltering())
+            {
+                Console.Write(element + ", ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
